a29de662384d283803c29d56c3817c04a3bbc81d
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2023-03-29 14:56:18 +0200

    Get ready to use picocli

---
+++ picocli-demo/src/test/java/dk/jarry/spring/playground/picoclidemo/PicocliDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.jarry.spring.playground.picoclidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicocliDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
